1b50aee85b453b1566481275d54d7faa2c43bfc2
Vinicch <vinicch.leme@gmail.com>
2024-05-19 20:19:28 -0300

    initial commit

---
+++ setup.sh
@@ -0,0 +1,32 @@
+echo "Updating mirrors..."
+sudo pacman -S reflector
+sudo reflector -c Brazil,Chile,Colombia -p https --sort rate --save /etc/pacman.d/mirrorlist
+
+echo
+echo "Installing packages..."
+sudo pacman -Syu
+sudo pacman -S alacritty base-devel bash-completion bat btop docker docker-buildx \
+    docker-compose fd firefox git go less man-db neovim noto-fonts noto-fonts-cjk \
+    noto-fonts-emoji ripgrep starship tar terraform ttf-noto-nerd unzip wget wl-clipboard zip
+
+echo
+echo "Installing Brave Browser through the AUR..."
+cd $HOME && git clone https://aur.archlinux.org/brave-bin.git
+cd $HOME/brave-bin && makepkg -si
+cd $HOME && rm -rf brave-bin/
+cd $HOME && git clone https://aur.archlinux.org/yay.git
+cd $HOME/yay && makepkg -si
+cd $HOME && rm -rf yay/
+
+echo
+echo "Installing Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo
+echo "Configuring Docker..."
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+
+echo
+echo "All done!"
