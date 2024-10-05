7816c3ba6657277031a7256e444e2e29c8e8e168
Vinicch <vinicch.leme@gmail.com>
2024-10-05 18:45:39 -0300

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 echo "Updating mirrors..."
 sudo pacman -S reflector
-sudo reflector -c Brazil,Chile,Colombia -p https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector -p https --sort score --save /etc/pacman.d/mirrorlist
 
 echo
 echo "Installing packages..."
 sudo pacman -Syu
 sudo pacman -S alacritty base-devel bash-completion bat btop docker docker-buildx \
     docker-compose fd firefox git go less man-db neovim noto-fonts noto-fonts-cjk \
-    noto-fonts-emoji ripgrep starship tar terraform ttf-noto-nerd unzip wget wl-clipboard zip
+    noto-fonts-emoji ripgrep spectacle starship tar ttf-noto-nerd unzip wget wl-clipboard zip
 
 echo
 echo "Installing Brave Browser through the AUR..."
@@ -24,8 +24,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo
 echo "Configuring Docker..."
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
 
 echo
