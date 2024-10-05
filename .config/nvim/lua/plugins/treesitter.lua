f58e2cb305cadadda20554f559d5fecbfdea6916
Vinicch <vinicch.leme@gmail.com>
2024-10-05 18:45:39 -0300

    update

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        vim.filetype.add({ extension = { templ = "templ" } })
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
                 "bash", "c", "css", "dockerfile", "go",
                 "html", "javascript", "json", "lua", "markdown",
-                "python", "rust", "sql", "templ", "terraform",
-                "toml", "typescript", "vimdoc", "vim", "yaml"
+                "python", "rust", "sql", "terraform", "toml",
+                "typescript", "vimdoc", "vim", "yaml"
             },
             highlight = { enable = true },
         })
